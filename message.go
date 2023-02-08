0ffcfce01330d17d82b33dad91cb7d7f4480e820
Wojciech Kwolek <wojciech@kwolek.io>
2023-02-08 23:52:01 +0100

    intital commit

---
+++ message.go
@@ -0,0 +1,17 @@
+package wsrpc
+
+type messageWrapper struct {
+	Type    string      `json:"type"`
+	Message interface{} `json:"message"`
+}
+
+type Message interface {
+	Type() string
+}
+
+func (c *Conn) SendMessage(m Message) error {
+	return c.SendRaw(messageWrapper{
+		Type:    m.Type(),
+		Message: m,
+	})
+}
